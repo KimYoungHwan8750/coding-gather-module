784a4584d6a31399cb670174aebde8991151690e
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-25 03:29:07 +0900

    각 WebsocketEvent에 대한 Response 타입 정의

--- src/response-type.ts
+++ src/response-type.ts
@@ -1,4 +1,15 @@
+import { LanguageType } from "./constant";
+import { ChangeLanguagePayload, InputTextPayload } from "./payload-type";
+
 export type SearchResponse = {
   code: 200 | 404;
   data: string;
 }
+
+export type InputTextResponse = {
+  language: LanguageType;
+} & InputTextPayload
+
+export type ChangeLanguageResponse = {
+
+} & ChangeLanguagePayload
