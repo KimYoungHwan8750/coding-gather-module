0a76d2a7015b890a7be46918cd46c4aba78d36a2
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-26 02:58:25 +0900

    리덕스 상태관리를 위해 불변객체 생성 메서드 작성

--- src/room-data.ts
+++ src/room-data.ts
@@ -1,5 +1,4 @@
-import { ChangeLanguagePayload, InputTextPayload } from "./payload-type";
-import { AppConstant, Direction, DirectionType, Language, LanguageType, Tools, ToolsType } from "./constant"
+import { AppConstant, DirectionType, LanguageType, Tools, ToolsType } from "./constant"
 
 export class EditorData {
 
@@ -35,12 +34,25 @@ export class EditorData {
     `text: ${this.text}`;
   }
 
+  withLanguage(language: LanguageType) {
+    return new EditorData(this.direction, language, this.text);
+  }
+
+  withText(text: string) {
+    return new EditorData(this.direction, this.language, text);
+  }
+
+  withDirection(direction: DirectionType) {
+    return new EditorData(direction, this.language, this.text);
+  }
+
 }
 
 export class CanvasData {
   private url: string = "";
-  private binary: number[] = [];
+  private binary: Uint8Array = Uint8Array.from([]);
   private tool: ToolsType = Tools.CURSOR;
+  private pending: boolean = false;
 
   getUrl() {
     return this.url;
@@ -58,7 +70,7 @@ export class CanvasData {
     this.url = url;
   }
 
-  setBinary(binary: number[]) {
+  setBinary(binary: Uint8Array) {
     this.binary = binary;
   }
 
@@ -67,9 +79,53 @@ export class CanvasData {
     return tool;
   }
 
+  isPending() {
+    return this.pending;
+  }
+
+  setPending(pending: boolean) {
+    this.pending = pending;
+  }
+
   toString() {
     return `url: ${this.url}\n` +
     `tools: ${this.tool}`;
+  }
+
+  withUrl(url: string) {
+    const canvas = new CanvasData();
+    canvas.setUrl(url);
+    canvas.setBinary(this.binary);
+    canvas.setTool(this.tool);
+    canvas.setPending(this.pending);
+    return canvas;
+  }
+
+  withBinary(binary: Uint8Array) {
+    const canvas = new CanvasData();
+    canvas.setUrl(this.url);
+    canvas.setBinary(binary);
+    canvas.setTool(this.tool);
+    canvas.setPending(this.pending);
+    return canvas;
+  }
+
+  withTool(tool: ToolsType) {
+    const canvas = new CanvasData();
+    canvas.setUrl(this.url);
+    canvas.setBinary(this.binary);
+    canvas.setTool(tool);
+    canvas.setPending(this.pending);
+    return canvas;
+  }
+
+  withPending(pending: boolean) {
+    const canvas = new CanvasData();
+    canvas.setUrl(this.url);
+    canvas.setBinary(this.binary);
+    canvas.setTool(this.tool);
+    canvas.setPending(pending);
+    return canvas;
   }
 }
 
@@ -80,4 +136,8 @@ export type EditorDatasType = {
 export class EditorDatas{
   topEditorData: EditorData = new EditorData(AppConstant.direction.TOP, AppConstant.language.PLAIN_TEXT, "");
   bottomEditorData: EditorData = new EditorData(AppConstant.direction.BOTTOM, AppConstant.language.PLAIN_TEXT, "");
+
+  getCorrectDirectionEditor(direction: DirectionType): EditorData {
+    return direction === AppConstant.direction.TOP ? this.topEditorData : this.bottomEditorData;
+  }
 }
